26438c1e429f9e38df47fc78724ef066ed32a740
VishwasNain <vishwasnain12@gmail.com>
2025-08-08 10:58:55 +0530

    forth commit

--- config/supabaseClient.js
+++ config/supabaseClient.js
@@ -2,12 +2,12 @@ const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error('Missing Supabase URL or API key in environment variables');
+if (!supabaseUrl || !supabaseServiceRoleKey) {
+  throw new Error('Missing Supabase URL or Service Role Key in env variables');
 }
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseServiceRoleKey);
 
 module.exports = supabase;
